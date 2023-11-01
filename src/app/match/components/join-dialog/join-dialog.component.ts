8eec0ecd585c7543f3859207cd3d3744c3a61ea0
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-11-01 14:41:47 -0500

    💄 Add validation styles

--- src/app/match/components/join-dialog/join-dialog.component.ts
+++ src/app/match/components/join-dialog/join-dialog.component.ts
@@ -1,9 +1,42 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'match-join-dialog',
   templateUrl: './join-dialog.component.html',
-  styleUrls: ['./join-dialog.component.scss']
+  styleUrls: ['./join-dialog.component.scss'],
 })
 export class JoinDialogComponent {
+  form = new FormGroup({
+    name: new FormControl('', {
+      nonNullable: true,
+      updateOn: 'blur',
+      validators: [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(20),
+        Validators.pattern(/^[a-zA-Z0-9]+$/),
+      ],
+    }),
+    mode: new FormControl<string>('', {
+      validators: [Validators.required],
+      nonNullable: true,
+    }),
+  });
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+
+    this.form.markAllAsTouched();
+  }
+
+  get name() {
+    return this.form.get('name')!;
+  }
+
+  get mode() {
+    return this.form.get('mode')!;
+  }
 }
--- src/app/match/match.module.ts
+++ src/app/match/match.module.ts
@@ -4,16 +4,10 @@ import { CommonModule } from '@angular/common';
 import { MatchRoutingModule } from './match-routing.module';
 import { JoinDialogComponent } from './components/join-dialog/join-dialog.component';
 import { MatchComponent } from './match.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    JoinDialogComponent,
-    MatchComponent
-  ],
-  imports: [
-    CommonModule,
-    MatchRoutingModule
-  ]
+  declarations: [JoinDialogComponent, MatchComponent],
+  imports: [CommonModule, MatchRoutingModule, ReactiveFormsModule],
 })
-export class MatchModule { }
+export class MatchModule {}
