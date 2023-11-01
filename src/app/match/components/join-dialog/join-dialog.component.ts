83d7d7480b851241d804e767ff1af143fdc8275a
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-11-01 11:04:41 -0500

    🚧 Create match module

---
+++ src/app/match/components/join-dialog/join-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'match-join-dialog',
+  templateUrl: './join-dialog.component.html',
+  styleUrls: ['./join-dialog.component.scss']
+})
+export class JoinDialogComponent {
+
+}
--- src/app/match/match.module.ts
+++ src/app/match/match.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatchRoutingModule } from './match-routing.module';
+import { JoinDialogComponent } from './components/join-dialog/join-dialog.component';
+import { MatchComponent } from './match.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    JoinDialogComponent,
+    MatchComponent
+  ],
   imports: [
     CommonModule,
     MatchRoutingModule
