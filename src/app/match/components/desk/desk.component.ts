72bd0a6c83e9c2db0a76862d03761419e7431ac1
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-11-01 15:57:30 -0500

    💄 Add desk component and styles, update match module

---
+++ src/app/match/components/desk/desk.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'match-desk',
+  templateUrl: './desk.component.html',
+  styleUrls: ['./desk.component.scss'],
+})
+export class DeskComponent {}
--- src/app/match/match.module.ts
+++ src/app/match/match.module.ts
@@ -5,9 +5,10 @@ import { MatchRoutingModule } from './match-routing.module';
 import { JoinDialogComponent } from './components/join-dialog/join-dialog.component';
 import { MatchComponent } from './match.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DeskComponent } from './components/desk/desk.component';
 
 @NgModule({
-  declarations: [JoinDialogComponent, MatchComponent],
+  declarations: [JoinDialogComponent, MatchComponent, DeskComponent],
   imports: [CommonModule, MatchRoutingModule, ReactiveFormsModule],
 })
 export class MatchModule {}
