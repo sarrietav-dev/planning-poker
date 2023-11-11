960bf90b89ab107e9a0ebca9e10f5713ff111288
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-11-11 01:23:30 -0500

    Add socket.io server for real-time chat functionality

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,11 +1,23 @@
 import Express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 const app = Express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.get("/", (_, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => {
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(3000, () => {
   console.log("Server running on port 3000");
 });
