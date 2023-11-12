e657e84c6e2cf54d719ae7e9f0debf0516287b71
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-11-12 15:37:59 -0500

    Add match creation endpoint and join match functionality.

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,19 +1,26 @@
 import Express from "express";
 import { Server, Socket } from "socket.io";
 import { createServer } from "http";
+import { nanoid } from "nanoid";
 
 const app = Express();
 const server = createServer(app);
 const io = new Server(server);
 
+app.post("/api/match", (_, res) => {
+  const matchId = nanoid();
+  const adminId = nanoid();
+
+  return res.json({ id: matchId, adminId });
+});
+
 io.on("connection", onConnection);
 
 function onConnection(socket: Socket) {
-  socket.on("chat message", forwardMessage);
-}
-
-function forwardMessage(msg: any) {
-  io.emit("chat message", msg);
+  socket.on(JoinMatchCommand, (matchId: string) => {
+    socket.join(matchId);
+    socket.emit("joined-match", matchId);
+  });
 }
 
 server.listen(3000, () => {
