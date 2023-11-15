37e0ef0c5f66fe92dd25c032f2ba9fe2f6041c4f
Sebastian <25210925+sarrietav-dev@users.noreply.github.com>
2023-11-15 18:52:49 -0500

    Add CreateMatchCommand and MatchCreated event to index.ts

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,18 +2,15 @@ import Express from "express";
 import { Server, Socket } from "socket.io";
 import { createServer } from "http";
 import { nanoid } from "nanoid";
-import { JoinMatchCommand } from "@planning-poker/events";
+import {
+  CreateMatchCommand,
+  JoinMatchCommand,
+  MatchCreated,
+} from "@planning-poker/events";
 
 const app = Express();
 const server = createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
-
-app.post("/api/match", (_, res) => {
-  const matchId = nanoid();
-  const adminId = nanoid();
-
-  return res.json({ id: matchId, adminId });
-});
 
 io.on("connection", onConnection);
 io.on("disconnect", () => console.log("Client disconnected"));
@@ -22,10 +19,13 @@ function onConnection(socket: Socket) {
   console.log("New connection");
 
   socket.on(JoinMatchCommand, (matchId: string) => {
-    console.log("Joining match", matchId);
-
     socket.join(matchId);
     socket.emit("joined-match", matchId);
+  });
+
+  socket.on(CreateMatchCommand, () => {
+    const matchId = nanoid();
+    socket.emit(MatchCreated, matchId);
   });
 }
 
