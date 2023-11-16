b9423af068d6fcb0974a2c916e6fb2edf440408f
Sebastian <25210925+sarrietav-dev@users.noreply.github.com>
2023-11-15 21:18:51 -0500

    Add Redis client connection and export function

--- apps/server/src/db/redis.ts
+++ apps/server/src/db/redis.ts
@@ -2,8 +2,16 @@ import { createClient } from "redis";
 
 const client = createClient();
 
+client.on("connect", () => {
+  console.log("Redis client connected");
+});
+
 client.on("error", (err) => {
   console.log("Error " + err);
 });
+
+export function connectClient() {
+  client.connect();
+}
 
 export default client;
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,11 +2,12 @@ import Express from "express";
 import { Server } from "socket.io";
 import { createServer } from "http";
 import initializeMethods from "./socket";
-import { Socket } from "dgram";
+import { connectClient as connectRedisClient } from "./db/redis";
 
 const app = Express();
 const server = createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
+connectRedisClient();
 
 io.on("connection", initializeMethods);
 io.on("disconnect", () => console.log("Client disconnected"));
