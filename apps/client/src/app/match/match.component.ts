d94cec0e74bd7bbe17a3661c73234e7a8d3ed772
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-11-16 20:06:07 -0500

    Refactor template to loop throw a list of players

--- apps/client/src/app/match/match.component.ts
+++ apps/client/src/app/match/match.component.ts
@@ -6,4 +6,35 @@ import { Component } from '@angular/core';
 })
 export class MatchComponent {
   isUserChosed: boolean = false;
+  currentUserIndex: number = 6;
+  players = [
+    {
+      name: 'Player 1',
+    },
+    {
+      name: 'Player 2',
+    },
+    {
+      name: 'Player 3',
+    },
+    {
+      name: 'Player 4',
+    },
+    {
+      name: 'Player 5',
+    },
+    {
+      name: 'Player 6',
+    },
+    {
+      name: 'Player 7',
+    },
+    {
+      name: 'Player 8',
+    },
+  ];
+
+  getSeatClass(index: number) {
+    return `seat seat--${index + 1}`
+  }
 }
