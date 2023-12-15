b3cdc75838f2b72868414a4316e6738b952aeca0
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-12-14 23:52:42 -0500

    Add tests and update component logic

--- apps/client/src/app/components/card/card.component.spec.ts
+++ apps/client/src/app/components/card/card.component.spec.ts
@@ -8,7 +8,7 @@ describe('CardComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [CardComponent]
+      declarations: [CardComponent],
     });
     fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
@@ -17,5 +17,28 @@ describe('CardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have default values', () => {
+    expect(component.selected).toBeUndefined();
+    expect(component.value).toBe('');
+  });
+
+  it('should emit select event when onSelectedCard is called', () => {
+    spyOn(component.select, 'emit');
+    component.onSelectedCard();
+    expect(component.select.emit).toHaveBeenCalled();
+  });
+
+  it('should update selected value when @Input selected is changed', () => {
+    component.selected = true;
+    fixture.detectChanges();
+    expect(component.selected).toBe(true);
+  });
+
+  it('should update value when @Input value is changed', () => {
+    component.value = 'test';
+    fixture.detectChanges();
+    expect(component.value).toBe('test');
   });
 });
