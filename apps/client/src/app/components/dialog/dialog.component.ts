81b3561655f5db1ddef035e0882daaafe9384f89
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-12-14 20:01:16 -0500

    Create components

---
+++ apps/client/src/app/components/dialog/dialog.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dialog',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dialog.component.html',
+  styleUrl: './dialog.component.scss'
+})
+export class DialogComponent {
+
+}
