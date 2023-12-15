2193e7a7b8bf18f6d5111f0ef28c2bd5d052dbb7
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-12-15 01:22:32 -0500

    Add Socket and Store spies to MatchService spec

--- apps/client/src/app/services/match/match.service.spec.ts
+++ apps/client/src/app/services/match/match.service.spec.ts
@@ -2,6 +2,8 @@ import { TestBed } from '@angular/core/testing';
 
 import { MatchService } from './match.service';
 import { Socket } from 'ngx-socket-io';
+import { Store } from '@ngrx/store';
+import { EMPTY } from 'rxjs';
 
 describe('MatchService', () => {
   let service: MatchService;
@@ -9,9 +11,19 @@ describe('MatchService', () => {
   beforeEach(() => {
     const socketSpy = jasmine.createSpyObj('Socket', [
       'emit',
+      'fromEvent',
     ]) as jasmine.SpyObj<Socket>;
+
+    socketSpy.fromEvent.and.callFake((event) => {
+      return EMPTY;
+    });
+
+    const storeSpy = jasmine.createSpyObj('Store', ['select', 'dispatch']);
     TestBed.configureTestingModule({
-      providers: [{ provide: Socket, useValue: socketSpy }],
+      providers: [
+        { provide: Socket, useValue: socketSpy },
+        { provide: Store, useValue: storeSpy },
+      ],
     });
     service = TestBed.inject(MatchService);
   });
