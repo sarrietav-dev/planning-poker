8f6c05737561121d5b202d1c6c61b1dbae3ba874
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-12-14 20:02:33 -0500

    Rename input to text-input

--- apps/client/src/app/components/text-input/text-input.component.spec.ts
+++ apps/client/src/app/components/text-input/text-input.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { InputComponent } from './input.component';
+import { TextInputComponent } from './text-input.component';
 
-describe('InputComponent', () => {
-  let component: InputComponent;
-  let fixture: ComponentFixture<InputComponent>;
+describe('TextInputComponent', () => {
+  let component: TextInputComponent;
+  let fixture: ComponentFixture<TextInputComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [InputComponent]
+      imports: [TextInputComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(InputComponent);
+    fixture = TestBed.createComponent(TextInputComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- apps/client/src/app/components/text-input/text-input.component.ts
+++ apps/client/src/app/components/text-input/text-input.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-input',
+  selector: 'app-text-input',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './input.component.html',
-  styleUrl: './input.component.scss'
+  templateUrl: './text-input.component.html',
+  styleUrl: './text-input.component.scss'
 })
-export class InputComponent {
+export class TextInputComponent {
 
 }
