4d5ded0a42ca4ba4e53032b40fac1a488513dad0
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-12-14 23:49:48 -0500

    Refactor card deck component to use dynamic card generation

--- apps/client/src/app/modules/match/components/card-deck/card-deck.component.ts
+++ apps/client/src/app/modules/match/components/card-deck/card-deck.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatchService } from 'src/app/services/match/match.service';
+import { reduce } from 'rxjs';
 
 @Component({
   selector: 'match-card-deck',
   templateUrl: './card-deck.component.html',
   styleUrl: './card-deck.component.scss',
 })
-export class CardDeckComponent {}
+export class CardDeckComponent {
+  constructor(private service: MatchService) {}
+
+  selectedCard = -1;
+
+  cardDeck$ = this.service.cardDeck$.pipe(
+    reduce((acc, curr) => {
+      return [...acc, curr];
+    }, [] as number[])
+  );
+
+  onSelectedCard(card: number) {
+    if (this.selectedCard === -1) this.selectedCard = card;
+  }
+}
