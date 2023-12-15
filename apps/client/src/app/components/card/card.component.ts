c6d3aa700e24997b418de5c920728484bdc4a96b
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-12-15 01:29:34 -0500

    Fix default value assignment for 'value' input in CardComponent

--- apps/client/src/app/components/card/card.component.ts
+++ apps/client/src/app/components/card/card.component.ts
@@ -10,7 +10,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class CardComponent {
   @Input() selected?: boolean;
-  @Input() value!: string;
+  @Input() value: string = '';
   @Output() select = new EventEmitter();
 
   onSelectedCard() {
