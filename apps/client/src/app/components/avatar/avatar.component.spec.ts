38f600193d5071036550a61d6e7e330e21773ec0
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-12-15 00:42:27 -0500

    Fix Avatar, Card and App tests

--- apps/client/src/app/components/avatar/avatar.component.spec.ts
+++ apps/client/src/app/components/avatar/avatar.component.spec.ts
@@ -8,7 +8,7 @@ describe('AvatarComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AvatarComponent]
+      imports: [AvatarComponent],
     });
     fixture = TestBed.createComponent(AvatarComponent);
     component = fixture.componentInstance;
@@ -17,5 +17,24 @@ describe('AvatarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return customPrefix if defined', () => {
+    component.customPrefix = 'CP';
+    expect(component.namePrefix).toBe('CP');
+  });
+
+  it('should return first two characters of name if customPrefix is not defined', () => {
+    component.name = 'Avatar';
+    expect(component.namePrefix).toBe('Av');
+  });
+
+  it('should return empty string if customPrefix and name are not defined', () => {
+    expect(component.namePrefix).toBe('');
+  });
+
+  it('should return correct background color if backgroundColor is default', () => {
+    component.backgroundColor = 'default';
+    expect(component.bgColor).toBe('var(--somewhat-blue)');
   });
 });
