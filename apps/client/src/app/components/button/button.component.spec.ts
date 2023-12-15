8c3a430dd465ae48b7909cb7c0ed28b9929f2bb3
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-12-14 23:03:02 -0500

    Refactor button component tests

--- apps/client/src/app/components/button/button.component.spec.ts
+++ apps/client/src/app/components/button/button.component.spec.ts
@@ -8,10 +8,9 @@ describe('ButtonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ButtonComponent]
-    })
-    .compileComponents();
-    
+      imports: [ButtonComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +18,27 @@ describe('ButtonComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have default values', () => {
+    expect(component.color).toBe('primary');
+    expect(component.variant).toBeUndefined();
+    expect(component.disabled).toBe(false);
+    expect(component.type).toBe('button');
+  });
+
+  it('should generate className correctly', () => {
+    component.color = 'secondary';
+    component.variant = 'outlined';
+    expect(component.className).toBe('btn btn--secondary btn--outlined');
+
+    component.variant = undefined;
+    expect(component.className).toBe('btn btn--secondary ');
+  });
+
+  it('should emit onClick event when handleClick is called', () => {
+    spyOn(component.onClick, 'emit');
+    component.handleClick();
+    expect(component.onClick.emit).toHaveBeenCalled();
   });
 });
