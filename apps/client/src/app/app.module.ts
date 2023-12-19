e0455aa7482aade521d39e4593203d62d63754ab
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-12-19 18:05:21 -0500

    Rename folders

--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CreateMatchFormComponent } from './modules/create-match-form/create-match-form.component';
+import { CreateMatchFormComponent } from './pages/create/create-match-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SocketIoModule } from 'ngx-socket-io';
 import { environment } from 'src/environments/environment';
