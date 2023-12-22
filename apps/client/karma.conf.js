f5b7ec650342a8f665b3407975fc06b85c600280
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-12-21 19:35:55 -0500

    Fix Karma Mocha reporter

--- apps/client/karma.conf.js
+++ apps/client/karma.conf.js
@@ -3,16 +3,16 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage"),
+      require("@angular-devkit/build-angular/plugins/karma"),
+      require("karma-mocha-reporter"),
     ],
-    reporters: ['mocha'],
     client: {
       jasmine: {
         // you can add configuration options for Jasmine here
@@ -20,21 +20,18 @@ module.exports = function (config) {
         // for example, you can disable the random execution with `random: false`
         // or set a specific seed with `seed: 4321`
       },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/pragma-poker'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+      dir: require("path").join(__dirname, "./coverage/pragma-poker"),
+      subdir: ".",
+      reporters: [{ type: "html" }, { type: "text-summary" }],
     },
-    reporters: ['progress', 'kjhtml'],
-    browsers: ['ChromeHeadless'],
-    restartOnFileChange: true
+    reporters: ["mocha"],
+    browsers: ["ChromeHeadless"],
+    restartOnFileChange: true,
   });
 };
