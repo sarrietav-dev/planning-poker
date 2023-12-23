44a865f1b0c89884be3af7145ef6f628f8ab5fcf
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-12-23 16:14:33 -0500

    Refactor match.guard.spec.ts and match.guard.ts

--- apps/client/src/app/guards/match/match.guard.spec.ts
+++ apps/client/src/app/guards/match/match.guard.spec.ts
@@ -1,14 +1,12 @@
-import {TestBed} from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
-import {MatchGuard} from './match.guard';
-import {Socket} from 'ngx-socket-io';
-import {Router, RouterStateSnapshot} from '@angular/router';
-import {Observable} from 'rxjs';
-import {MatchService} from 'src/app/services/match/match.service';
+import { Socket } from 'ngx-socket-io';
+import { Router, RouterStateSnapshot } from '@angular/router';
+import { MatchService } from 'src/app/services/match/match.service';
+import { MatchGuard } from './match.guard';
 
 describe('MatchGuard', () => {
-  let guard: MatchGuard;
-  let socketService: jasmine.SpyObj<Socket>;
+  let guard: typeof MatchGuard;
   let routerService: jasmine.SpyObj<Router>;
   let service: jasmine.SpyObj<MatchService>;
 
@@ -27,14 +25,13 @@ describe('MatchGuard', () => {
 
     TestBed.configureTestingModule({
       providers: [
-        {provide: Socket, useValue: socketSpy},
-        {provide: Router, useValue: routerSpy},
-        {provide: MatchService, useValue: serviceSpy},
+        { provide: Socket, useValue: socketSpy },
+        { provide: Router, useValue: routerSpy },
+        { provide: MatchService, useValue: serviceSpy },
       ],
     });
-    guard = TestBed.inject(MatchGuard);
+    guard = MatchGuard;
     routerService = TestBed.inject(Router) as jasmine.SpyObj<Router>;
-    socketService = TestBed.inject(Socket) as jasmine.SpyObj<Socket>;
     service = TestBed.inject(MatchService) as jasmine.SpyObj<MatchService>;
   });
 
@@ -42,50 +39,49 @@ describe('MatchGuard', () => {
     expect(guard).toBeTruthy();
   });
 
-  it('should navigate to root and return false if matchId is not defined', (done) => {
+  it('should navigate to root and return false if matchId is not defined', async () => {
     const route = {
-      paramMap: {get: jasmine.createSpy().and.returnValue(null)},
+      paramMap: { get: jasmine.createSpy().and.returnValue(null) },
     } as any;
     const state = {} as RouterStateSnapshot;
-    const result = guard.canActivate(route, state) as Observable<boolean>;
+    const result = await TestBed.runInInjectionContext(
+      () => guard(route, state) as Promise<boolean>
+    );
 
     expect(routerService.navigate).toHaveBeenCalledWith(['/']);
 
-    result.subscribe((value) => {
-      expect(value).toBe(false);
-      done();
-    });
+    expect(result).toBe(false);
   });
 
-  it('should navigate to root and return false if match does not exist', (done) => {
+  it('should navigate to root and return false if match does not exist', async () => {
     const route = {
-      paramMap: {get: jasmine.createSpy().and.returnValue('123')},
+      paramMap: { get: jasmine.createSpy().and.returnValue('123') },
     } as any;
 
     service.doesMatchExist.and.returnValue(Promise.resolve(false));
 
     const state = {} as RouterStateSnapshot;
-    const result = guard.canActivate(route, state) as Observable<boolean>;
+    const result = await TestBed.runInInjectionContext(
+      () => guard(route, state) as Promise<boolean>
+    );
 
-
-    result.subscribe((value) => {
-      expect(routerService.navigate).toHaveBeenCalled();
-      expect(value).toBe(false);
-      done();
-    });
+    expect(routerService.navigate).toHaveBeenCalled();
+    expect(result).toBe(false);
   });
 
-  it('should return true if match exists', (done) => {
+  it('should return true if match exists', async () => {
     const route = {
-      paramMap: {get: jasmine.createSpy().and.returnValue('123')},
+      paramMap: { get: jasmine.createSpy().and.returnValue('123') },
     } as any;
     const state = {} as RouterStateSnapshot;
+
     service.doesMatchExist.and.returnValue(Promise.resolve(true));
-    const result = guard.canActivate(route, state) as Observable<boolean>;
+
+    const result = await TestBed.runInInjectionContext(
+      () => guard(route, state) as Promise<boolean>
+    );
+
     expect(routerService.navigate).not.toHaveBeenCalled();
-    result.subscribe((value) => {
-      expect(value).toBe(true);
-      done();
-    });
+    expect(result).toBe(true);
   });
 });
