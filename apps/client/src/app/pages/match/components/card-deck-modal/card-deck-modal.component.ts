d44061dc6bbda3a975c3bac5528d32242c7980e7
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-12-26 20:11:18 -0500

    Add properties and methods to card deck modal

--- apps/client/src/app/pages/match/components/card-deck-modal/card-deck-modal.component.ts
+++ apps/client/src/app/pages/match/components/card-deck-modal/card-deck-modal.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatchService } from 'src/app/services/match/match.service';
+import { toArray } from 'rxjs';
 
 @Component({
   selector: 'match-card-deck-modal',
   templateUrl: './card-deck-modal.component.html',
-  styleUrl: './card-deck-modal.component.scss'
+  styleUrl: './card-deck-modal.component.scss',
 })
 export class CardDeckModalComponent {
+  constructor(private service: MatchService) {}
 
+  @Input() choosenCard: number = -1;
+  @Output() choosenCardChange = new EventEmitter<number>();
+
+  cards: number[] = [];
+
+  ngOnInit() {
+    this.service
+      .cardDeck$()
+      .pipe(toArray())
+      .subscribe((cards) => {
+        this.cards = cards;
+      });
+  }
+
+  chooseCard(card: number) {
+    this.choosenCardChange.emit(card);
+  }
 }
