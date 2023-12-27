96aadbb27f2192178520e5412ca91c95b4881607
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-12-26 20:46:00 -0500

    Update isUserChosed to false

--- apps/client/src/app/pages/match/match.component.ts
+++ apps/client/src/app/pages/match/match.component.ts
@@ -15,14 +15,14 @@ export class MatchComponent implements OnInit {
     private route: ActivatedRoute,
     private store: Store<{
       match: State;
-    }>
+    }>,
   ) {
     this.getSpectators$().subscribe((spectators) => {
       this.spectators$ = spectators;
     });
   }
 
-  isUserChosed: boolean = true;
+  isUserChosed: boolean = false;
   match$ = this.store.select('match');
   isInviteModalOpen = false;
   spectators$: State['match']['spectators'] = [];
@@ -37,7 +37,7 @@ export class MatchComponent implements OnInit {
   get spectatorsCount$() {
     return this.matchService.getSpectators().pipe(
       switchMap((spectators) => from(spectators)),
-      scan((acc) => acc + 1, 0)
+      scan((acc) => acc + 1, 0),
     );
   }
 
@@ -62,7 +62,7 @@ export class MatchComponent implements OnInit {
     this.matchService.joinMatch(
       this.route.snapshot.paramMap.get('id')!,
       data.name,
-      data.mode
+      data.mode,
     );
   }
 
@@ -81,7 +81,11 @@ export class MatchComponent implements OnInit {
     }
   }
 
+  get isUserPlayer() {
+    return true;
+  }
+
   get isCardDeckModalOpen() {
-    return this.selectedCard === -1 && this.isUserChosed;
+    return this.selectedCard === -1 && this.isUserChosed && this.isUserPlayer;
   }
 }
