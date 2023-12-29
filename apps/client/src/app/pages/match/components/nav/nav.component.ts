7efccf3bfd3459a029353c45a7aa7a28c0e3bad2
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-12-29 11:43:05 -0500

    Move nav to new nav component

---
+++ apps/client/src/app/pages/match/components/nav/nav.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'match-nav',
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.scss',
+})
+export class NavComponent {}
