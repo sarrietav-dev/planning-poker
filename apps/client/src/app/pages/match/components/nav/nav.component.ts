a14f1eca2b8c62bb3f8f99a7a1e5a2320eb456c8
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-12-31 00:23:01 -0500

    Extract nav into its own component
    * Move nav to new nav component
    
    * Refactor match component and nav component
    
    * Refactor NavComponent to use Input and Output decorators
    
    * Refactor match component and nav component

---
+++ apps/client/src/app/pages/match/components/nav/nav.component.ts
@@ -0,0 +1,49 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs';
+import { MatchService } from 'src/app/services/match/match.service';
+import { State } from 'src/app/store';
+
+@Component({
+  selector: 'match-nav',
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.scss',
+})
+export class NavComponent implements OnInit {
+  constructor(
+    private store: Store<{
+      match: State;
+    }>,
+    private matchService: MatchService,
+  ) {}
+
+  ngOnInit(): void {
+    this.getSpectators$().subscribe((spectators) => {
+      this.spectators$ = spectators;
+    });
+  }
+
+  match$ = this.store.select('match');
+  spectators$: State['match']['spectators'] = [];
+  spectatorsCount = 0;
+  @Input() isInviteModalOpen = false;
+  @Output() isInviteModalOpenChange = new EventEmitter<boolean>();
+
+  get name$() {
+    return this.match$.pipe(map((match) => match.match.name));
+  }
+
+  getSpectators$() {
+    return this.matchService
+      .getSpectators()
+      .pipe(map((spectators) => spectators.slice(0, 3)));
+  }
+
+  get spectatorCountLabel() {
+    return `${this.spectatorsCount - 3}+`;
+  }
+
+  handleInviteModalOpen() {
+    this.isInviteModalOpenChange.emit(true);
+  }
+}
