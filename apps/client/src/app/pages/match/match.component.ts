16088ac0cf7f6d9a575cc8aff84a229c110044b6
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2024-01-02 18:55:16 -0500

    Fix card deck modal condition

--- apps/client/src/app/pages/match/match.component.ts
+++ apps/client/src/app/pages/match/match.component.ts
@@ -13,7 +13,7 @@ export class MatchComponent implements OnInit {
     private route: ActivatedRoute,
   ) {}
 
-  isUserChosed: boolean = true;
+  isUserChosed: boolean = false;
   match?: Match = undefined;
   isInviteModalOpen = false;
   selectedCard = -1;
@@ -25,7 +25,7 @@ export class MatchComponent implements OnInit {
     });
 
     this.matchService.getAreCardsRevealed().subscribe((areCardsRevealed) => {
-      this.areCardsRevealed = !areCardsRevealed ?? false;
+      this.areCardsRevealed = areCardsRevealed ?? false;
     });
   }
 
