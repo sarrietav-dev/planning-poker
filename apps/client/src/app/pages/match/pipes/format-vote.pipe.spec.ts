f7581792c53a4c4140fc69585d6bd500d883bdfc
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2024-01-04 16:02:40 -0500

    Add unit tests for FormatVotePipe

--- apps/client/src/app/pages/match/pipes/format-vote.pipe.spec.ts
+++ apps/client/src/app/pages/match/pipes/format-vote.pipe.spec.ts
@@ -5,4 +5,28 @@ describe('FormatVotePipe', () => {
     const pipe = new FormatVotePipe();
     expect(pipe).toBeTruthy();
   });
+
+  describe('FormatVotePipe', () => {
+    let pipe: FormatVotePipe;
+
+    beforeEach(() => {
+      pipe = new FormatVotePipe();
+    });
+
+    it('create an instance', () => {
+      expect(pipe).toBeTruthy();
+    });
+
+    it('should return "1 voto" when value is 1', () => {
+      expect(pipe.transform(1)).toEqual("1 voto");
+    });
+
+    it('should return "2 votos" when value is 2', () => {
+      expect(pipe.transform(2)).toEqual("2 votos");
+    });
+
+    it('should return "0 votos" when value is 0', () => {
+      expect(pipe.transform(0)).toEqual("0 votos");
+    });
+  });
 });
