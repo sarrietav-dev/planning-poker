deb19514321092af159d2f09a5aaec918c49eb74
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2024-01-10 21:18:22 -0500

    Add onRevealCards event handler

--- apps/server/src/event-handlers/index.ts
+++ apps/server/src/event-handlers/index.ts
@@ -2,3 +2,4 @@ export { default as createMatch } from "./create-match";
 export { default as joinMatch } from "./join-match";
 export { default as onDoesMatchExist } from "./does-match-exist";
 export { default as onChooseCard } from "./choose-card";
+export { default as onRevealCards } from "./reveal-cards";
---
+++ apps/server/src/event-handlers/reveal-cards/reveal-cards.test.ts
@@ -0,0 +1,62 @@
+import {test, describe, expect, vi, beforeEach} from 'vitest'
+import * as repo from "../../db/repository";
+import * as events from "@planning-poker/events";
+import onResetGame from './index';
+import {afterEach, mock} from "node:test";
+
+vi.mock("../../db/repository");
+
+const mockRepo = vi.mocked(repo);
+
+describe('On reset game tests', () => {
+
+    beforeEach(() => {
+        vi.clearAllMocks();
+    })
+
+    test('restarts game if the user is an admin', async () => {
+        const mockSocket = {
+            data: {
+                userId: 'testUserId',
+            },
+            rooms: {
+                values: vi.fn().mockReturnValue({
+                    next: vi.fn().mockReturnValue({value: 'testMatchId'}),
+                }),
+            },
+            to: vi.fn().mockReturnThis(),
+            emit: vi.fn(),
+        }
+        mockRepo.isMatchAdmin.mockResolvedValue(true);
+
+        await onResetGame(mockSocket as any, 'testMatchId');
+
+        expect(mockRepo.resetGame).toHaveBeenCalledWith('testMatchId');
+        expect(mockSocket.to).toHaveBeenCalledWith('testMatchId');
+        expect(mockSocket.emit).toHaveBeenCalledWith(events.MatchRestarted);
+    })
+
+    test('does not restart game if the user is not an admin', async () => {
+        const mockSocket = {
+            data: {
+                userId: 'testUserId',
+            },
+            rooms: {
+                values: vi.fn().mockReturnValue({
+                    next: vi.fn().mockReturnValue({value: 'testMatchId'}),
+                }),
+            },
+            to: vi.fn().mockReturnThis(),
+            emit: vi.fn(),
+        }
+
+        mockRepo.isMatchAdmin.mockResolvedValue(false);
+
+        await onResetGame(mockSocket as any, 'testMatchId');
+
+        expect(mockRepo.resetGame).not.toHaveBeenCalled();
+        expect(mockSocket.to).not.toHaveBeenCalled();
+        expect(mockSocket.emit).not.toHaveBeenCalled();
+    })
+
+})
